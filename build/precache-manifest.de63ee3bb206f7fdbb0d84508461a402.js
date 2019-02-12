f096af054f14ca0b9c44a1a4c15770530955908b
pp250046 <prudhvi.potnuru@teradata.com>
2019-02-12 12:31:13 +0530

    first commit

---
+++ build/precache-manifest.de63ee3bb206f7fdbb0d84508461a402.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db940631fd075c06bb83",
+    "url": "/static/css/main.688774e6.chunk.css"
+  },
+  {
+    "revision": "db940631fd075c06bb83",
+    "url": "/static/js/main.db940631.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0e166512a363bc9a5e6e",
+    "url": "/static/js/2.0e166512.chunk.js"
+  },
+  {
+    "revision": "cfe385755aa7d602a60fda990c808e46",
+    "url": "/index.html"
+  }
+];
